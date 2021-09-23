cdabece4e8f383ec60e17828a731c6b84eaa0499
Alexandre <alexandre.lemas.91@gmail.com>
2021-09-23 09:28:24 +0200

    [ADD]: Front form new post

--- groupomania/src/screens/NewPost.js
+++ groupomania/src/screens/NewPost.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import Axios from 'axios';
 
 import Header from '../components/Header'
 
 const NewPost = () => {
+    const history = useHistory()
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
@@ -38,6 +41,7 @@ const NewPost = () => {
                 }
               }) .then(res => {
             console.log(res.data);
+            history.push('/')
             })
             .catch(err => {
                 alert(err.response.data.message)
@@ -59,35 +63,58 @@ const NewPost = () => {
     return(
         <>
         <Header/>
-        <h1>Nouveau POST</h1>
-        <form className='newPost__form' onSubmit={e => {
-            e.preventDefault()
-            submit()
-        }}>
-            <input className='newPost__form__inp' type='text' placeholder='Titre (obligatoire)' value={title} onChange={e => setTitle(e.target.value)} required/>
-            <textarea className='newPost__form__inp' type='text' placeholder='Description (facultative)' value={description} onChange={e => setDescription(e.target.value)}/>
-            <input 
-            required
-            accept='image/*'
-            multiple={false} 
-            className='newPost__form__inp' 
-            type='file' 
-            onChange={e => {
-                let file = e.target.files[0];
+        <section className='newPost'>
+            <h1 className='newPost__title'>Nouveau POST</h1>
+            <form className='newPost__form' onSubmit={e => {
+                e.preventDefault()
+                submit()
+            }}>
+                <input 
+                className='newPost__form__title'
+                type='text' placeholder='Titre (obligatoire)' 
+                value={title} onChange={e => setTitle(e.target.value)} 
+                required/>
 
-                if(file && file.type.substr(0, 5) === 'image'){
-                    setImage(file)
-                } else {
-                    setImage(null)
+                <textarea 
+                maxLength={280}
+                className='newPost__form__description' 
+                type='text' 
+                placeholder='Description (facultative)' 
+                value={description} 
+                onChange={e => setDescription(e.target.value)}/>
+
+                <div>
+
+                <input 
+                required
+                style={{marginBottom: 20}}
+                accept='image/*'
+                multiple={false} 
+                type='file' 
+                onChange={e => {
+                    let file = e.target.files[0];
+
+                    if(file && file.type.substr(0, 5) === 'image'){
+                        setImage(file)
+                    } else {
+                        setImage(null)
+                    }
+                }}/>
+                </div>
+
+
+                {preview !== "" ? 
+                <img style={{width: 400, height: 400, objectFit: 'cover'}} src={preview} />
+                :
+                <div className='newPost__form__fakeImg'/>
                 }
-            }}/>
-            {preview !== "" ? 
-            <img style={{width: 400, height: 400, objectFit: 'cover'}} src={preview} />
-            :
-            null
-            }
-            <button className='newPost__form__inp' type='submit' >Valider</button>
-        </form>
+
+                <button className='newPost__form__submit' type='submit' >Poster</button>
+
+            </form>
+
+        </section>
+
         </>
 
     )
