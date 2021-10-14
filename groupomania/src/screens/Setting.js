fb99066e5856fda328253ee710aa68a5f720aa90
Alexandre <alexandre.lemas.91@gmail.com>
2021-10-14 10:56:11 +0200

    [ADD]: Create Setting screen

---
+++ groupomania/src/screens/Setting.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "../components/Header";
+
+const Setting = () => {
+    return (
+        <>
+            <Header />
+            <h1>Paramètres</h1>
+        </>
+    )
+}
+
+export default Setting
