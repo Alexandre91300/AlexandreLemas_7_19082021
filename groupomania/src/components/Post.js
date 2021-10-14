f9f6f66c4d45b460f6adcf1405baa9e33d5f2458
Alexandre <alexandre.lemas.91@gmail.com>
2021-10-14 09:07:43 +0200

    [ADD]: Delete & Modify post in Home

--- groupomania/src/components/Post.js
+++ groupomania/src/components/Post.js
@@ -6,7 +6,7 @@ import { useHistory } from 'react-router';
 import Modal from './Modal';
 import { useEffect, useState } from 'react';
 
-const Post = ({post, type}) => {
+const Post = ({ post }) => {
     const history = useHistory();
 
     const [displayModal, setDisplayModal] = useState(false)
@@ -22,135 +22,116 @@ const Post = ({post, type}) => {
     const postId = post.id;
 
     useEffect(() => {
-        if(post.likes.split(' ').find(e => e === uid)){
+        if (post.likes.split(' ').find(e => e === uid)) {
             setPostLiked(true)
         }
 
         console.log(post.likes.split(' '));
-    
-        if(post.likes.split(' ')[0] === '0'){
+
+        if (post.likes.split(' ')[0] === '0') {
             setlikeNumber(likeNumber - 1)
         }
 
-    },[])
+    }, [])
 
     const deletePost = () => {
         console.log('Delete post');
 
         let imageUrl = post.image;
 
-        if (token && uid && postId && imageUrl){
+        if (token && uid && postId && imageUrl) {
 
             // Send request
-            Axios.post('http://localhost:3000/api/posts/delete', {postId : postId, imageUrl : imageUrl}, {
+            Axios.post('http://localhost:3000/api/posts/delete', { postId: postId, imageUrl: imageUrl }, {
                 headers: {
-                  authorization: uid + ' ' + token
+                    authorization: uid + ' ' + token
                 }
-              }).then(res => {
+            }).then(res => {
                 window.location.reload()
             })
-            .catch(err => {
-                console.log(err.response.data.message)
-            })
+                .catch(err => {
+                    console.log(err.response.data.message)
+                })
         }
     }
 
     const toggleLikePost = () => {
         console.log('Like post');
 
-        if (token && uid && postId ){
+        if (token && uid && postId) {
 
             // Send request
-            Axios.post('http://localhost:3000/api/posts/like', {postId : postId, uid : uid}, {
+            Axios.post('http://localhost:3000/api/posts/like', { postId: postId, uid: uid }, {
                 headers: {
-                  authorization: uid + ' ' + token
+                    authorization: uid + ' ' + token
                 }
-              }).then(res => {
+            }).then(res => {
                 setPostLiked(!postLiked)
 
-                if (!postLiked){
+                if (!postLiked) {
                     setlikeNumber(likeNumber + 1)
                 } else {
                     setlikeNumber(likeNumber - 1)
                 }
 
             })
-            .catch(err => {
-                console.log(err.response.data.message)
-            })
+                .catch(err => {
+                    console.log(err.response.data.message)
+                })
         }
     }
 
     const modifyPost = () => {
         history.push({
-            pathname : '/modifyPost',
+            pathname: '/modifyPost',
             state: {
-                title : post.title,
-                description : post.description,
-                image : post.image,
-                postId : post.id
+                title: post.title,
+                description: post.description,
+                image: post.image,
+                postId: post.id
             }
         })
     }
 
-    switch (type) {
-        case 'profil':
-            return(
-                <div className='post'>
-                    <h2 className='post__title'>{post.title}</h2>
-                    <p className='post__description'>{post.description}</p>
-                    <div>
-                        <button className='post__deleteBtn' onClick={() => deletePost()} >Supprimer</button>
-                        <button className='post__modifyBtn' onClick={() => modifyPost()} >Modifier</button>
-                    </div>
-                    <img className='post__image' src={post.image} alt='Comment'/>
-                    <p className='post__txt'>{post.username} <span className='post__txt--grey'>le {date}</span></p>
-                    <div className='post__ctn'>
-                        <div onClick={() => toggleLikePost()}>
-                        {postLiked ?
-                            <img className='post__ctn__icon' src={likeBlack} alt='Like'/>
-                            :
-                            <img className='post__ctn__icon' src={like} alt='Like'/>
-                            }
-                        </div>
-                        <p className='post__ctn__txtLike'>{likeNumber} Like</p>
-                    </div>
-                    <div className='post__ctn'>
-                        <img className='post__ctn__icon' src={comment} alt='Comment'/>
-                        <p className='post__ctn__txtComment'>{post.commentaires} Commentaires</p>
-                    </div>
+    console.log(post.uid);
+    console.log(uid);
+
+
+    return (
+        <div className='post'>
+            <h2 className='post__title'>{post.title}</h2>
+            <p className='post__description'>{post.description}</p>
+            {uid == post.uid ?
+                <div>
+                    <button className='post__deleteBtn' onClick={() => deletePost()} >Supprimer</button>
+                    <button className='post__modifyBtn' onClick={() => modifyPost()} >Modifier</button>
                 </div>
-            )
-    
-        default:
-            return(
-                <div className='post'>
-                    <h2 className='post__title'>{post.title}</h2>
-                    <p className='post__description'>{post.description}</p>
-                    <img className='post__image' src={post.image} alt='Comment'/>
-                    <p className='post__txt'>{post.username} <span className='post__txt--grey'>le {date}</span></p>
-                    <div className='post__ctn'>
-                        <div onClick={() => toggleLikePost()}>
-                        {postLiked ?
-                            <img className='post__ctn__icon' src={likeBlack} alt='Like'/>
-                            :
-                            <img className='post__ctn__icon' src={like} alt='Like'/>
-                            }
-                        </div>
-                        <p className='post__ctn__txtLike'>{likeNumber} Like</p>
-                    </div>
-                    <div className='post__ctn' onClick={() => setDisplayModal(true)}>
-                        <img className='post__ctn__icon' src={comment} alt='Comment'/>
-                        <p className='post__ctn__txtComment'>{post.commentaires} Commentaires</p>
-                    </div>
-                    {displayModal && post.comments !== 0 ? 
-                    <Modal post={post} callBack={() => {setDisplayModal(false)}} />
-                    :
-                    null
+                :
+                null
+            }
+            <img className='post__image' src={post.image} alt='Comment' />
+            <p className='post__txt'>{post.username} <span className='post__txt--grey'>le {date}</span></p>
+            <div className='post__ctn'>
+                <div onClick={() => toggleLikePost()}>
+                    {postLiked ?
+                        <img className='post__ctn__icon' src={likeBlack} alt='Like' />
+                        :
+                        <img className='post__ctn__icon' src={like} alt='Like' />
                     }
                 </div>
-            )
-    }
+                <p className='post__ctn__txtLike'>{likeNumber} Like</p>
+            </div>
+            <div className='post__ctn' onClick={() => setDisplayModal(true)}>
+                <img className='post__ctn__icon' src={comment} alt='Comment' />
+                <p className='post__ctn__txtComment'>{post.commentaires} Commentaires</p>
+            </div>
+            {displayModal && post.comments !== 0 ?
+                <Modal post={post} callBack={() => { setDisplayModal(false) }} />
+                :
+                null
+            }
+        </div>
+    )
 
 }
 
--- groupomania/src/screens/Profil.js
+++ groupomania/src/screens/Profil.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Axios from 'axios';
 
 import Header from "../components/Header";
@@ -28,52 +28,52 @@ const Profil = () => {
 
             console.log('send request');
             // Send request
-            Axios.post('http://localhost:3000/api/posts/getByUid',{uid : uid}, {
-              headers: {
-                authorization: uid + ' ' + token
-              }
+            Axios.post('http://localhost:3000/api/posts/getByUid', { uid: uid }, {
+                headers: {
+                    authorization: uid + ' ' + token
+                }
             }).then(res => {
-    
-            console.log(res.data.posts);
 
-            if(res.data.posts !== undefined){
+                console.log(res.data.posts);
 
-                setAllPosts(res.data.posts)
-            }
+                if (res.data.posts !== undefined) {
+
+                    setAllPosts(res.data.posts)
+                }
             })
-            .catch(err => {
-                console.log(err);
-            })
-        }
-
-    },[])
-    return(
-        <>
-        <Header/>
-        <section className="profil__header">
-            <img src={user_black} className="profil__header__img" alt='Icon utilisateur'/>
-            {username ? <h1 className="profil__header__title">{username}</h1> : <h1 className="profil__header__title">Username introuvable</h1>}
-        <button 
-        data-testid='accueil-btn-newPost'
-        className='accueil__btn' 
-        onClick={() => {
-            history.push('/newPost')
-        }}>Nouveau Post</button>
-            <span className="profil__header__line"/>
-        </section>
-
-        { allPosts.length > 0 ?
-        <section className='profil__allPost'>
-            {
-                allPosts.map(e => {
-                    return <Post type='profil' post={e} key={e.id} />
+                .catch(err => {
+                    console.log(err);
                 })
-            }
-        </section>
-        :
-        <h2 style={{width: '100%', textAlign: "center"}}>Aucun post</h2>
-
         }
+
+    }, [])
+    return (
+        <>
+            <Header />
+            <section className="profil__header">
+                <img src={user_black} className="profil__header__img" alt='Icon utilisateur' />
+                {username ? <h1 className="profil__header__title">{username}</h1> : <h1 className="profil__header__title">Username introuvable</h1>}
+                <button
+                    data-testid='accueil-btn-newPost'
+                    className='accueil__btn'
+                    onClick={() => {
+                        history.push('/newPost')
+                    }}>Nouveau Post</button>
+                <span className="profil__header__line" />
+            </section>
+
+            {allPosts.length > 0 ?
+                <section className='profil__allPost'>
+                    {
+                        allPosts.map(e => {
+                            return <Post post={e} key={e.id} />
+                        })
+                    }
+                </section>
+                :
+                <h2 style={{ width: '100%', textAlign: "center" }}>Aucun post</h2>
+
+            }
 
         </>
     )
